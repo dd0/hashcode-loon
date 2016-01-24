e8fdd20f35d1217b11d20998b9a5fcdcae71c407
dd0 <de298@cam.ac.uk>
2016-01-24 21:43:11 +0000

    random stuff

--- main.cpp
+++ main.cpp
@@ -1,6 +1,7 @@
 #include "didi.h"
 #include "file.h"
 #include "data.h"
+<<<<<<< cae46c492f792786cf38d79e535a575bb4017824
 #include "walker.h"
 #include "state.h"
 
@@ -17,6 +18,7 @@ int main()
     srand(time(0));
     printf("MAIN\n"); fflush(stdout);
 	loadData("loon.in");
+<<<<<<< cae46c492f792786cf38d79e535a575bb4017824
 	cout << data.n << " " << data.m << endl;
 	cout << data.time << endl;
 	cout << data.baloons;
@@ -30,5 +32,6 @@ int main()
 
     balon=balon.second.generate_random_walker(10,10,-1,400,*stanje);
     delete stanje;
+
     return 0;
 }
---
+++ visuals.h
@@ -0,0 +1,13 @@
+#ifndef VISUALS_H
+#define VISUALS_H
+
+#include "CImg.h"
+#include "data.h"
+#include <iostream>
+
+using namespace cimg_library;
+
+const int DRAW_T = 4;
+void drawTargets();
+
+#endif
