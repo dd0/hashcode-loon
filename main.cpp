cb37af6127acd0813a26849df59ffc3a75a34625
dd0 <de298@cam.ac.uk>
2016-01-24 21:50:06 +0000

    Merging fix

--- main.cpp
+++ main.cpp
@@ -1,7 +1,6 @@
 #include "didi.h"
 #include "file.h"
 #include "data.h"
-<<<<<<< cae46c492f792786cf38d79e535a575bb4017824
 #include "walker.h"
 #include "state.h"
 
@@ -18,7 +17,6 @@ int main()
     srand(time(0));
     printf("MAIN\n"); fflush(stdout);
 	loadData("loon.in");
-<<<<<<< cae46c492f792786cf38d79e535a575bb4017824
 	cout << data.n << " " << data.m << endl;
 	cout << data.time << endl;
 	cout << data.baloons;
